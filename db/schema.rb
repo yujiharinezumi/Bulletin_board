bb44dc80f327d9c67f34e487ebe0efb74a279405
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-02-14 11:10:09 +0900

    post コントローラとモデルを作成,newのviewを作成しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_003504) do
+ActiveRecord::Schema.define(version: 2020_02_14_012656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "text"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
