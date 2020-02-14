8cc2944281acf7949b29093c97c0ea9cc02ecf36
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-02-14 15:16:56 +0900

    カテゴリーとポスト、カテゴライズでの多対多のアソシエーションを組みました。

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :categorizes, dependent: :destroy
+  has_many :posts, through: :categorize
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :categorizes, dependent: :destroy
+  has_many :categories, through: :categorizes
   validates :text, presence: true
 end
