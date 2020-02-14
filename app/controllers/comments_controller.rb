721aef97d51f5bff82963a445857daff591a99e5
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-02-14 12:15:52 +0900

    comment モデル、コントローラーを作成 create機能を追加しました。

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def create
+    
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_path(@post) }
+      else
+        format.html { redirect_to post_path(@post), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :content)
+  end
+end
