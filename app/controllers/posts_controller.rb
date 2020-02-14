d1000122d3762413214de1c1c60041f9c47d61fe
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-02-14 11:17:58 +0900

    postのcreate機能を追加しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,20 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    Post.create(post_params)
+    redirect_to new_post_path
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text)
   end
 end
