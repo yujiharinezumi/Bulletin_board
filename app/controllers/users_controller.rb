bfc3b813ac34cbc949e887bcfb698d2224428710
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-02-14 14:29:41 +0900

    userの編集、更新機能を追加しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user,only: [:show,:edit,:update]
   def new
     @user = User.new
   end
@@ -14,13 +15,27 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+   def update
+     if @user.update(user_params)
+       redirect_to posts_path,notice:"ユーザー情報を編集しました！"
+     else
+       render :edit
+     end
+   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users
 end
