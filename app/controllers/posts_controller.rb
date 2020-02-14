36028b2407bf0255e7248c5217c64a79ba92d800
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-02-14 11:35:35 +0900

    post edit機能を追加しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,10 +17,20 @@ class PostsController < ApplicationController
   end
 
   def show
-     @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+     @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to posts_path, notice: "投稿を編集しました"
+      else
+        render :edit
+      end
   end
 
   private
