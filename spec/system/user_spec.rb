47ab628ad3d2bc69a408b8d03841f3069c14f6f6
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-02-16 20:57:38 +0900

    ログイン、ログアウトのシステムスペックを追加しました。

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -52,6 +52,32 @@ RSpec.describe 'Users', type: :system do
       click_button 'commit'
       expect(page).to have_content 'aのページ'
     end
+  end
 
+  describe 'ログインのテスト' do
+    before do
+      @user1 = FactoryBot.create(:user_first)
+      @user2 = FactoryBot.create(:user_second)
+    end
+
+    it 'ログイン失敗のテスト' do
+      visit new_session_path
+      fill_in('session_email',with: "aa@example.com")
+      fill_in('session_password', with:'aaaaaa')
+      click_button 'commit'
+      expect(page).to have_content 'ログインに失敗しました'
+    end
+
+    it 'ログイン成功のテスト' do
+      visit new_session_path
+      log_in @user1
+      expect(page).to have_content '掲示板一覧'
+    end
+
+    it 'ログアウトのテスト' do
+      log_in @user1
+      click_on 'Logout'
+      expect(page).to have_content 'ログアウトしました'
+    end
   end
 end
