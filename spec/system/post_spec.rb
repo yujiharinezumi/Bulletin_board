cda4d27f2d5e1f5c4ac80b295654a7a7bc978fba
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-02-16 21:52:11 +0900

    Postのシステムスペックを追加しました。

---
+++ spec/system/post_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+require 'support/utilities'
+
+RSpec.describe 'Posts', type: :system do
+  before do
+    @user1 = FactoryBot.create(:user_first)
+    @user2 = FactoryBot.create(:user_second)
+    @post1 = FactoryBot.create(:post_first, user_id: @user1.id)
+    log_in @user1
+  end
+
+  it 'ユーザーが投稿するテスト' do
+    click_on "掲示板投稿する"
+    fill_in('post_text',with:"hello")
+    click_on 'commit'
+    expect(page).to have_content('hello')
+  end
+
+  it '投稿失敗するテスト' do
+    click_on "掲示板投稿する"
+    fill_in('post_text',with:"")
+    click_on 'commit'
+    expect(page).to have_content('Textを入力してください')
+  end
+
+  it '投稿の編集が失敗するテスト' do
+    click_on "投稿の編集する"
+    page.driver.browser.switch_to.alert.accept
+    fill_in('post_text',with:"bbbbbbb")
+    click_on 'commit'
+    expect(page).to have_content('投稿を編集しました。')
+    expect(page).to have_content('bbbbbbb')
+  end
+
+  it 'ユーザーが投稿を削除するテスト' do
+    click_on "投稿を削除する"
+    page.driver.browser.switch_to.alert.accept
+    expect(page).to have_content('投稿を削除しました。')
+    expect(page).not_to have_content('AAA')
+  end
+end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -2,83 +2,83 @@ require 'rails_helper'
 require 'support/utilities'
 
 RSpec.describe 'Users', type: :system do
-  # describe '新規登録画面' do
-  #   it '名前が空欄でエラーメッセージが出るテスト' do
-  #     visit root_path
-  #     fill_in('user_name', with: '')
-  #     fill_in('user_email', with: 'a@gmail.com')
-  #     fill_in('user_password', with: 'aaaaaa')
-  #     fill_in('user_password_confirmation', with: 'aaaaaa')
-  #     click_button 'commit'
-  #     expect(page).to have_content('Nameを入力してください')
-  #   end
-  #
-  #   it 'emailが空欄でエラーメッセージが出るテスト' do
-  #     visit root_path
-  #     fill_in('user_name', with: 'a')
-  #     fill_in('user_email', with: '')
-  #     fill_in('user_password', with: 'aaaaaa')
-  #     fill_in('user_password_confirmation', with: 'aaaaaa')
-  #     click_button 'commit'
-  #     expect(page).to have_content('Emailを入力してください')
-  #  end
-  #
-  #   it 'passwordが空欄でエラーメッセージが出るテスト' do
-  #     visit root_path
-  #     fill_in('user_name', with: 'a')
-  #     fill_in('user_email', with: 'a@gmail.com')
-  #     fill_in('user_password', with: '')
-  #     fill_in('user_password_confirmation', with: 'aaaaaa')
-  #     click_button 'commit'
-  #     expect(page).to have_content('Passwordを入力してください')
-  #   end
-  #
-  #   it 'password_confirmationが空欄でエラーメッセージが出るテスト' do
-  #     visit root_path
-  #     fill_in('user_name', with: 'a')
-  #     fill_in('user_email', with: 'a@gmail.com')
-  #     fill_in('user_password', with: 'aaaaaa')
-  #     fill_in('user_password_confirmation', with: '')
-  #     click_button 'commit'
-  #     expect(page).to have_content('Password confirmationとPasswordの入力が一致しません')
-  #   end
-  #
-  #   it 'ユーザーの新規登録のテスト' do
-  #     visit root_path
-  #     fill_in('user_name',with: 'a')
-  #     fill_in('user_email', with: 'a@gmail.com')
-  #     fill_in('user_password', with: 'aaaaaa')
-  #     fill_in('user_password_confirmation', with: 'aaaaaa')
-  #     click_button 'commit'
-  #     expect(page).to have_content 'aのページ'
-  #   end
-  # end
+  describe '新規登録画面' do
+    it '名前が空欄でエラーメッセージが出るテスト' do
+      visit root_path
+      fill_in('user_name', with: '')
+      fill_in('user_email', with: 'a@gmail.com')
+      fill_in('user_password', with: 'aaaaaa')
+      fill_in('user_password_confirmation', with: 'aaaaaa')
+      click_button 'commit'
+      expect(page).to have_content('Nameを入力してください')
+    end
 
-  # describe 'ログインのテスト' do
-  #   before do
-  #     @user1 = FactoryBot.create(:user_first)
-  #   end
-  #
-  #   it 'ログイン失敗のテスト' do
-  #     visit new_session_path
-  #     fill_in('session_email',with: "aa@example.com")
-  #     fill_in('session_password', with:'aaaaaa')
-  #     click_button 'commit'
-  #     expect(page).to have_content 'ログインに失敗しました'
-  #   end
-  #
-  #   it 'ログイン成功のテスト' do
-  #     visit new_session_path
-  #     log_in @user1
-  #     expect(page).to have_content '掲示板一覧'
-  #   end
-  #
-  #   it 'ログアウトのテスト' do
-  #     log_in @user1
-  #     click_on 'Logout'
-  #     expect(page).to have_content 'ログアウトしました'
-  #   end
-  # end
+    it 'emailが空欄でエラーメッセージが出るテスト' do
+      visit root_path
+      fill_in('user_name', with: 'a')
+      fill_in('user_email', with: '')
+      fill_in('user_password', with: 'aaaaaa')
+      fill_in('user_password_confirmation', with: 'aaaaaa')
+      click_button 'commit'
+      expect(page).to have_content('Emailを入力してください')
+   end
+
+    it 'passwordが空欄でエラーメッセージが出るテスト' do
+      visit root_path
+      fill_in('user_name', with: 'a')
+      fill_in('user_email', with: 'a@gmail.com')
+      fill_in('user_password', with: '')
+      fill_in('user_password_confirmation', with: 'aaaaaa')
+      click_button 'commit'
+      expect(page).to have_content('Passwordを入力してください')
+    end
+
+    it 'password_confirmationが空欄でエラーメッセージが出るテスト' do
+      visit root_path
+      fill_in('user_name', with: 'a')
+      fill_in('user_email', with: 'a@gmail.com')
+      fill_in('user_password', with: 'aaaaaa')
+      fill_in('user_password_confirmation', with: '')
+      click_button 'commit'
+      expect(page).to have_content('Password confirmationとPasswordの入力が一致しません')
+    end
+
+    it 'ユーザーの新規登録のテスト' do
+      visit root_path
+      fill_in('user_name',with: 'a')
+      fill_in('user_email', with: 'a@gmail.com')
+      fill_in('user_password', with: 'aaaaaa')
+      fill_in('user_password_confirmation', with: 'aaaaaa')
+      click_button 'commit'
+      expect(page).to have_content 'aのページ'
+    end
+  end
+
+  describe 'ログインのテスト' do
+    before do
+      @user1 = FactoryBot.create(:user_first)
+    end
+
+    it 'ログイン失敗のテスト' do
+      visit new_session_path
+      fill_in('session_email',with: "aa@example.com")
+      fill_in('session_password', with:'aaaaaa')
+      click_button 'commit'
+      expect(page).to have_content 'ログインに失敗しました'
+    end
+
+    it 'ログイン成功のテスト' do
+      visit new_session_path
+      log_in @user1
+      expect(page).to have_content '掲示板一覧'
+    end
+
+    it 'ログアウトのテスト' do
+      log_in @user1
+      click_on 'Logout'
+      expect(page).to have_content 'ログアウトしました'
+    end
+  end
 
   describe 'ユーザーの編集、更新のテスト' do
     before do
