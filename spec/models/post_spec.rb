032871caa25c3602a2ab0db2918c94baec06f8c5
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-02-16 22:21:20 +0900

    postのmodel specを追加しました。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user_first)
+  end
+
+  it "textが空ならバリデーションが通らない" do
+    post = Post.new(text: '',user_id: @user.id)
+    expect(post).not_to be_valid
+    end
+
+  it "textが150文字以上ならバリデーションが通らない" do
+    post = Post.new(text: 'a'*151, user_id: @user.id )
+      expect(post).not_to be_valid
+    end
+
+  it "textが記載されている場合バリデーションが通る" do
+    post = Post.new( text: 'hellohello',user_id: @user.id)
+    expect(post).to be_valid
+  end
 end
